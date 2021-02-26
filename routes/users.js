d377bc4aa1592a653265dba8a385f06f91c6dce9
Swann Pincet <swann.pincet@gmail.com>
2021-02-26 17:40:20 +0100

    removed prefs and started to comment route mechanics

--- routes/users.js
+++ routes/users.js
@@ -5,22 +5,28 @@ const fileUpload = require('express-fileupload')
 
 //signup route, same used for 1st registering user then registering its preferences 
 router.post('/sign-up', (req,res) => {
-  //> here will be checked if user already exists and if password has sufficient strength
-  if (!req.body.isSettingPreferences) {
+  
+  //body : username, email, password, fav sports (array), handiSport, country (from geo)
+  //check if email already exists in DB
+  //check if password has sufficient strength with RegEx
+  //if all pass, bcrypt password
+  //then save user to DB, res.json a token for the local storage and send an email confirmation
 
-  } else {
-    
-  }
 })
 
 //basic sign-in
 router.post('/sign-in', (req,res) => {
+
+  //body : username or email and password
+  //check if user exists
+  // compare body password with encrypted one
+  // if pass, res.json a token for the local storage
   
 })
 
-//modif profil ( pas la photo de prof)
+//modif profil/preferences ( pas la photo de prof)
 router.put('/edit-profile', (req,res) => {
-  
+   // 
 })
 
 
