1499a7f719c10d5266713ccbc5a12f79417484e6
Swann Pincet <swann.pincet@gmail.com>
2021-03-01 17:51:00 +0100

    made filling route to include all sports and big cities in france

--- routes/helper_db.js
+++ routes/helper_db.js
@@ -90,42 +90,105 @@ const sportIds = [
     },
 ]
 
-const PlaygroundModel = require('./db/PlaygroundModel')
+const cities = [
+    {
+        name: "paris",
+        lat: 48.856614,
+        lon: 2.3522219
+    },
+    {
+        name: "marseille",
+        lat: 43.2961743,
+        lon: 5.3699525
+    },
+    {
+        name: "toulouse",
+        lat: 43.6044622,
+        lon: 1.4442469
+    },
+    {
+        name: "lyon",
+        lat: 45.7578137,
+        lon: 4.8320114
+    },
+    {
+        name: "nice",
+        lat: 43.7009358,
+        lon: 7.2683912
+    },
+    {
+        name: "nantes",
+        lat: 47.2186371,
+        lon: -1.5541362
+    },
+    {
+        name: "strasbourg",
+        lat: 48.584614,
+        lon: 7.7507127
+    },
+    {
+        name: "montpellier",
+        lat: 43.6112422,
+        lon: 3.8767337
+    },
+    {
+        name: "bordeaux",
+        lat: 44.841225,
+        lon: -0.5800364
+    },
+    {
+        name: "lille",
+        lat: 50.6365654,
+        lon: 3.0635282
+    },
+    {
+        name: "metz",
+        lat: 49.1196964,
+        lon: 6.1763552
+    },
+]
 
-const testUrl = "https://api.foursquare.com/v2/venues/search?client_id=ID0H1AIMM4ACISZJSL4LOHDEUROIBXYL1REZWETBZ0Q3XQ23&client_secret=WY2S0O3CSK5E1XAGEJ4GYE0V1VPLAR1MBBJE5KS1ORUF0DKW&v=20210215&ll=48.86076636445482, 2.3376654555775627&radius=3000&query=&categoryId=4bf58dd8d48988d1e1941735"
+const PlaygroundModel = require('./db/PlaygroundModel')
 
 router.get('/fill-playground-internal', async (req,res) => {
 
     await PlaygroundModel.deleteMany({}).then(() => console.log("Playground data cleared.")).catch(err => console.log(err))
 
     try {
-        const result = await request(testUrl)
-        const resultJson  = JSON.parse(result.body)
-        const playgrounds = resultJson.response.venues
+        for (let j = 0; j < cities.length; j++) {
+            for (let k = 0; k < sportIds.length;k++) {
+                const result = await request(`https://api.foursquare.com/v2/venues/search?client_id=ID0H1AIMM4ACISZJSL4LOHDEUROIBXYL1REZWETBZ0Q3XQ23&client_secret=WY2S0O3CSK5E1XAGEJ4GYE0V1VPLAR1MBBJE5KS1ORUF0DKW&v=20210215&ll=${cities[j].lat}, ${cities[j].lon}&radius=10000&query=&categoryId=${sportIds[k].id}`)
+                const resultJson  = JSON.parse(result.body)
+                const playgrounds = resultJson.response.venues
 
-        for (let i = 0; i< playgrounds.length;i++) {
+                if(playgrounds.length > 0) {
+                    for (let i = 0; i< playgrounds.length;i++) {
 
-            const e = playgrounds[i]
-            const reverseGeo = await request(`https://api-adresse.data.gouv.fr/reverse/?lon=${e.location.lng}&lat=${e.location.lat}`)
-            const reverseGeoJSON = JSON.parse(reverseGeo.body)
-
-            const newPlayground = new PlaygroundModel({
-                name: e.name,
-                location: {
-                    latitude: e.location.lat,
-                    longitude: e.location.lng
-                },
-                address: reverseGeoJSON.features[0].properties.label,
-                sports: e.categories.map(category => category.id),
-                affluence: undefined,
-                free: undefined,
-                public: undefined,
-                contact: [],
-                covering: undefined,
-                icons: e.categories.map(category => category.icon.prefix + category.icon.suffix)
-            })
-            await newPlayground.save()
+                        const e = playgrounds[i]
+                        const reverseGeo = await request(`https://api-adresse.data.gouv.fr/reverse/?lon=${e.location.lng}&lat=${e.location.lat}`)
+                        const reverseGeoJSON = JSON.parse(reverseGeo.body)
+    
+                        const newPlayground = new PlaygroundModel({
+                            name: e.name,
+                            location: {
+                                latitude: e.location.lat,
+                                longitude: e.location.lng
+                            },
+                            address: reverseGeoJSON.features[0].properties.label,
+                            sports: e.categories.map(category => category.id),
+                            affluence: undefined,
+                            free: undefined,
+                            public: undefined,
+                            contact: [],
+                            covering: undefined,
+                            icons: e.categories.map(category => category.icon.prefix + category.icon.suffix)
+                        })
+                    await newPlayground.save()
+                    }
+                }
+            }
         }
+
         console.log("Playgrounds DB has been filled.")
         res.send("Playgrounds DB has been filled.")
         
