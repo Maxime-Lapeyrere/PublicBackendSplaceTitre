188b8d3f6a9ea43f874743a428ca6a7df2b4541e
Swann Pincet <swann.pincet@gmail.com>
2021-03-01 10:47:08 +0100

    completed UserModel

--- routes/db/UserModel.js
+++ routes/db/UserModel.js
@@ -1,8 +1,33 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    exemple : String,
+    userName: {type: String, unique: true},
+    email: {type: String, unique: true},
+    password: String,
+    age: Number,
+    gender: String,
+    phoneNumber: String,
+    handiSport: Boolean,
+    geolocation: {
+        latitude: Number,
+        longitude: Number
+    },
+    premium: Boolean,
+    friendsList: [{type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
+    teams: [{type: mongoose.Schema.Types.ObjectId, ref: 'teams'}],
+    profilePicture: String,
+    bio: String,
+    joinedEvents: [{type: mongoose.Schema.Types.ObjectId, ref: 'events'}], //historique d'events et events à venir ici, traitement sera fait sur le back ou sur le front
+    country: String,
+    blockedUser: [{type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
+    favoriteSports: [String],
+    favoritePlaygrounds: [{type: mongoose.Schema.Types.ObjectId, ref: 'playgrounds'}],
+    club: [String], //(?)
+    language: String,
+    conversations: [{type: mongoose.Schema.Types.ObjectId, ref: 'conversations'}],
+    connectionToken: String, // to be used to authenticate through the app, also, will be stored in local storage
+    resetToken : String,
+    resetTokenExpirationDate: Date
 })
 
-
-const Model = mongoose.model('nameofcollection', userSchema);
+export default mongoose.model('users', userSchema);
