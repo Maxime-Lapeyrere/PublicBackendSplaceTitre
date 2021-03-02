7ce0e4fb5c389313be3ea79fef1eb5348955fdde
Swann Pincet <swann.pincet@gmail.com>
2021-03-02 21:02:56 +0100

    added community family route to clear up main route file

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.post('/get-shops', (req,res) => {
 //swipe
 router.get('/get-users', (req,res) => {
   //filters in query >> ie distance preferences
-  
+
 })
 
 //swipe
@@ -36,37 +36,6 @@ router.post('/like', (req,res)=> {
 //swipe
 router.post('/dislike', (req,res)=> {
   
-})
-
-
-//COMMUNITY
-// list d'amis, recherche user, and start conversation
-
-//generer la liste d'amis 
-router.post('/get-friends',(req,res) => {
-  //fetch friend list and conversations history from db
-})
-
-//ET l'historique de conversations
-router.post('/get-conversations-history', (req,res) => {
-
-})
-
-router.post('/search-user', (req,res) => {
-
-})
-
-router.post('/add-friend', (req,res) => {
-
-})
-
-//load conversation only when entering it
-router.post('/get-messages', (req,res) => {
-  //load conv with conv ID and user token for further security
-})
-
-router.post('/save-messages', (req,res) => {
-  //used in parallel of socket io route
 })
 
 
